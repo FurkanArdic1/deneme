847c4956a9299359243b57311e4873f650a88f4d
Muhammed Furkan ARDIÇ <72447903+FurkanArdic1@users.noreply.github.com>
2021-01-21 13:27:50 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="http://furkanardic.me/Style/style.css">
+    <title>LiveLab | İzle , takip et, eğlen !</title>
+</head>
+<body>
+    <header>
+    
+        <div class="container">
+              <div class="logo"> <h2 style="color: white;top:15px;position: absolute;">L  I  V  E  L  A  B</h2>  </div> 
+              <div class="search"><input type="text" name="search" id="search" placeholder="Type for search..."></div>
+            <div class="links">
+            
+                <ul>
+                
+                    <li><a href="">Anasayfa</a></li>
+                    <li><a href="">Ayarlar</a></li>
+                    <li><a href="">Yayın Aç</a></li>
+                    <li><a href="">Yayıncı Bul</a></li>
+                    <li><a href="">Kurallar</a></li>
+                    <li><a href="">Deluxe</a></li>
+                
+                
+                </ul>
+            
+            
+            </div>
+        
+        </div>
+    
+    
+    
+    
+    </header>
+</body>
+</html>
